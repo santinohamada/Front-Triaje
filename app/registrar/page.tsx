cf95b6504fb529193f3fab4c75f19dc80d312b37
SantinoHamada <hamadasantino@hotmail.com>
2025-11-21 00:40:07 -0300

    suspense para searchParams

--- app/registrar/page.tsx
+++ app/registrar/page.tsx
@@ -1,10 +1,15 @@
 import { RegisterForm } from '@/components/register-form';
+import { Suspense } from 'react';
 
 
 const page = () => {
     return (
        <div className="min-h-screen flex items-center justify-center bg-linear-to-br from-blue-50 to-cyan-50 dark:from-gray-900 dark:to-gray-800 p-4">
-            <RegisterForm />
+        <Suspense fallback={<div>Cargando...</div>}>
+        <RegisterForm />
+    </Suspense>
+
+            
         </div>
     );
 };
